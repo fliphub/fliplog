a3c11c4dcf3a7c09368fb6bd7c81ea370e11145b
Arete Code <aretecode@gmail.com>
2017-04-17 03:20:14 -0700

    📜⚙⚡📦⬇ dynamic deps solutions
      - 📜 prepublish script for resetting cache content
      - ⚡📦⬇ dynamic deps solutions with perf cache
      - 📘⚙⚡📦 add docs for dynamic dep configs
    
      - ⚙ add .config for reading pkg configs
      - 🍰 add presets variable for hardcoded config options
      - ⚡ more performant conditionals
      - ⚡ improve conditionals in echoing checking default data
      - 🆙 improve color formatting functionality
      - ⚡ improved perf in config loops
      - ⚡ reduce extension methods and memory overhead by not using .extends and instead  using the map.set where possible

---
+++ dynamicTags.js
@@ -0,0 +1,57 @@
+// @NOTE this file is not included in npm files
+const {execSync} = require('child_process')
+const {resolve} = require('path')
+const {existsSync, writeFileSync, readFileSync} = require('fs')
+let pkg = require('./package.json')
+
+const pkgPath = resolve(__dirname, './package.json')
+const ogPkg = JSON.stringify(pkg, null, 2)
+const ogPkgObj = JSON.parse(ogPkg)
+
+function dynamicTag(tag) {
+  try {
+    execSync(`echo "module.exports = '${tag}'" > ./tagged.js`, {stdio: 'inherit'})
+    pkg.version = ogPkgObj.version
+
+    // @NOTE: ignoring this for now,
+    // @example 0.2.0-beta.1
+    //
+    // so it would become
+    // 0.2.0-beta.1-cli
+    //
+    // if (pkg.version.includes('-')) {
+    //   pkg.version = pkg.version.split('-').shift()
+    // }
+
+    // add tag to version
+    pkg.version += '-' + tag
+
+    // save pkg
+    writeFileSync(pkgPath, JSON.stringify(pkg, null, 2), 'utf8')
+
+    console.log({tag, version: pkg.version})
+    console.log(`npm publish --tag ${tag}`)
+
+    // publish with tag
+    execSync(`npm publish --tag ${tag}`, {stdio: 'inherit'})
+  } catch (e) {
+    // restore original
+    writeFileSync(pkgPath, ogPkg, 'utf8')
+  }
+
+  // no need for this
+  // execSync(`npm version patch --no-git-tag-version && `)
+}
+
+function last() {
+  console.log('restoring')
+
+  // restore original
+  writeFileSync(pkgPath, ogPkg, 'utf8')
+
+  // empty the dynamicTag
+  execSync(`echo "module.exports = false" > ./tagged.js`, {stdio: 'inherit'})
+}
+
+['fun', 'formatting', 'debugging', 'cli'].forEach(tag => dynamicTag(tag))
+last()
