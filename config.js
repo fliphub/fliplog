d2c7d966942d9f9c4919609744923971f82cdbf4
Arete Code <aretecode@gmail.com>
2017-04-29 19:04:39 -0700

    ⚒⚙📦 fixes for config with dynamic deps

--- config.js
+++ config.js
@@ -1,17 +1,18 @@
 // this all takes only ~10 microseconds
 
 const pth = require('path')
-const rootp = require('app-root-path').path
+const rootp = require('app-root-path').path // eslint-disable-line
 
 let pkgPath = pth.resolve(rootp, './package.json')
-const pkg = require(pkgPath)
+let pkg = {}
+try {
+  pkg = require(pkgPath) || {} // eslint-disable-line
+}
+catch (e) {
+  // suckage
+}
+
 const tagged = require('./tagged')
-
-// console.log(pkg.version, pkg.name, pkg)
-// const pkg = require('read-pkg-up').sync()
-// const pkg = require('./package.json')
-
-// console.log(pkg.version, pkg.name, pkg)
 
 const presets = {
   min: {
@@ -37,6 +38,8 @@ const presets = {
       'sparkly',
       // (g)ui - extends cli
       'diff',
+      'beep',
+      'list',
     ],
   },
 
@@ -73,7 +76,6 @@ const presets = {
     middleware: [],
     extends: ['min', 'debugging', 'formatting'],
     plugins: [
-      'beep',
       'box',
       'notify',
       'progress',
@@ -91,6 +93,16 @@ const presets = {
       // 'story',
     ],
   },
+
+  default: {
+    middleware: [],
+    extends: ['min', 'cli', 'debugging', 'formatting'],
+    plugins: [
+      // todo
+      // 'list',
+      // 'story',
+    ],
+  },
 }
 
 // paths to require and use
@@ -99,85 +111,86 @@ let exporte = {exportee, pkg}
 let paths = []
 
 // if we've already run it
-if (exportee.length !== 0) {
-  module.exports = exporte
-  return
-}
+if (exportee.length === 0) {
+  function resolve(ex) {
+    const mware = presets[ex].middleware
+    const plugins = presets[ex].plugins
 
-function resolve(ex) {
-  const mware = presets[ex].middleware
-  const plugins = presets[ex].plugins
+    for (let m = 0; m < mware.length; m++) {
+      // uniq
+      const path = './middleware/' + mware[m]
+      if (paths.includes(path)) continue
 
-  for (let m = 0; m < mware.length; m++) {
-    // uniq
-    const path = './middleware/' + mware[m]
-    if (paths.includes(path)) continue
+      // resolve to path, require
+      exportee.push(require(path)) // eslint-disable-line
+      paths.push(path)
+    }
+    for (let p = 0; p < plugins.length; p++) {
+      // uniq
+      const path = './plugins/' + plugins[p]
+      if (paths.includes(path)) continue
 
-    // resolve to path, require
-    exportee.push(require(path))
-    paths.push(path)
-  }
-  for (let p = 0; p < plugins.length; p++) {
-    // uniq
-    const path = './plugins/' + plugins[p]
-    if (paths.includes(path)) continue
-
-    // resolve to path, require
-    exportee.push(require(path))
-    paths.push(path)
-  }
-}
-
-function remap(use) {
-  const xtends = presets[use].extends
-
-  for (let i = 0; i < xtends.length; i++) {
-    resolve(xtends[i])
+      // resolve to path, require
+      exportee.push(require(path)) // eslint-disable-line
+      paths.push(path)
+    }
   }
 
-  resolve(use)
-}
+  function remap(use) {
+    const xtends = presets[use].extends
 
-// always use .min
-remap('min')
+    for (let i = 0; i < xtends.length; i++) {
+      resolve(xtends[i])
+    }
 
-// remap('latest')
-// module.exports = exportee
-
-if (pkg.fliplog) {
-  let config = pkg.fliplog
-  if (Array.isArray(config) === false) {
-    config = config.use
+    resolve(use)
   }
 
-  for (let c = 0; c < config.length; c++) {
-    const use = config[c]
-    if (use === 'min') continue
-    if (presets[use] !== null && presets[use] !== undefined) {
-      if (pkg.fliplog.debug === true) {
-        console.log('using ', use)
+  // always use .min
+  remap('min')
+
+  // remap('latest')
+  // module.exports = exportee
+
+  if (pkg.fliplog) {
+    let config = pkg.fliplog
+    let debug = false
+    if (config !== undefined && config.debug !== undefined) {
+      debug = true
+    }
+    if (Array.isArray(config) === false) {
+      config = config.use
+    }
+
+    for (let c = 0; c < config.length; c++) {
+      const use = config[c]
+      if (use === 'min') continue
+      if (presets[use] !== null && presets[use] !== undefined) {
+        if (pkg.fliplog.debug === true) {
+          console.log('using ', use)
+        }
+
+        remap(use)
       }
-
-      remap(use)
-    }
-    else {
-      console.log('used an unknown fliplog config: ', {use})
+      else {
+        console.log('used an unknown fliplog config: ', {use})
+      }
     }
   }
-}
-else if (tagged === false) {
-  if (process.argv.includes('--fliplog') === true) {
-    console.log('has no tag and no pkg config, using "latest"')
-  }
+  else if (tagged === false) {
+    if (process.argv.includes('--fliplog') === true) {
+      console.log('has no tag and no pkg config, using "latest"')
+    }
 
-  remap('latest')
-}
-else {
-  if (pkg.fliplog.debug === true) {
-    console.log('has no package config, but has tag: ', tagged)
+    remap('latest')
   }
+  else {
+    if (pkg.fliplog !== undefined && pkg.fliplog.debug === true) {
+      console.log('has no package config, but has tag: ', tagged)
+    }
 
-  remap(tagged)
+    remap(tagged)
+  }
 }
 
 module.exports = exporte
