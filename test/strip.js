8de79845f2ee824e034a07f986d0181a1e50e422
Arete Code <aretecode@gmail.com>
2017-06-14 04:21:12 -0700

    🚫🎨 ansi-strip test

--- test/strip.js
+++ test/strip.js
@@ -1,9 +1,14 @@
-const log = (data, word) => {
-  const fliplog = require('../')
+const fliplog = require('../')
+
+const _log = (data, word) => {
   const stripAnsi = fliplog.requirePkg('strip-ansi')
   const {text, datas} = fliplog.text(word).prettyformat(data).returnVals()
   if (text) console.log(text)
   console.log(stripAnsi(datas))
 }
 
-log({user: true})
+// _log({user: true})
+
+
+fliplog.strip().bold('uncolor me').echo()
+fliplog.strip(fliplog.chalk().yellow('uncolor-me-too')).echo()
