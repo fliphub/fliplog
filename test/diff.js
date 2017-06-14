1eab913fe3a58725957782b6bf502ac2a7f663d7
Arete Code <aretecode@gmail.com>
2017-06-14 04:21:37 -0700

    ⚖️ more diff tests

--- test/diff.js
+++ test/diff.js
@@ -10,6 +10,17 @@ const two = {
 log.diff(eh, two).echo()
 log.diff('eh', 'ehoh').echo()
 
+const oneOne = {oneOne: 'was a race horse 🐎'}
+log.diff(oneOne)
+const twoTwo = Object.assign({twoTwo: 'was one too.'})
+log.diff(twoTwo)
+log.echo()
+
+let oneOneTwoTwo = 'one-one  was a race horse 🐎 '
+log.diff(oneOneTwoTwo)
+log.diff(oneOneTwoTwo + '... two-two was one, two.')
+log.echo()
+
 log
   .bold('💸\n')
   .tree({
